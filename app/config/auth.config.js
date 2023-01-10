37898b3b1ea99d2e5530871302d0f818df14d15d
tranminhbinh <tranminhbinhit@gmail.com>
2023-01-10 14:42:21 +0700

    no message

--- app/config/auth.config.js
+++ app/config/auth.config.js
@@ -1,9 +1,9 @@
 module.exports = {
   secret: "bezkoder-secret-key",
-  jwtExpiration: 86400, // 1 day
-  jwtRefreshExpiration: 7776000, // 3 month
+  // jwtExpiration: 86400, // 1 day
+  // jwtRefreshExpiration: 7776000, // 3 month
 
   /* for test */
-  // jwtExpiration: 60, // 1 minute
-  // jwtRefreshExpiration: 120, // 2 minutes
+  jwtExpiration: 60, // 1 minute
+  jwtRefreshExpiration: 86400, // 1 day
 };
